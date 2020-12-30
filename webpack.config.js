4ce020225b4208c4ba73557908ca1f83ec2faae1
Keven Fortin-Simard <keven@hotelwizards.com>
2020-12-29 20:13:04 -0500

    Add plugin, modify server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -8,5 +10,11 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader"
         }]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
